5915534845c60c548e4a413d3b5b225699bdd77c
Mike Rhodes <educated.fools@gmail.com>
2023-03-08 19:39:43 -0800

    finally understand calculate function

--- js/modules/calculator.js
+++ js/modules/calculator.js
@@ -1,24 +1,39 @@
 // ADD A PRIVATE FUNCTION CALLED CALCULATE
-//I'm confused about this. Isn't a private function essentially just a nested function?
-//I'm not understanding the purpose within the context of this assignment
-
-// ADD FOUR PUBLIC FUNCTIONS BELOW
+function calculate(num1, num2, operand){
+    switch(operand){
+        case 'add':
+            alert(num1 + num2);
+            break;
+        case 'subtract':
+            alert(num1 - num2);
+            break;
+        case 'multiply':
+            alert(num1 * num2);
+            break;
+        case 'divide':
+            alert(num1 / num2);
+            break;
+        default:
+            alert('You Blew It!');
+    }
+}
+// ADD FOUR PUBLIC FUNCTIONS BELOW-
 //////////////////////////////////
 // ADD FUNCTION
 const performAddition = (num1, num2) => {
-    alert(num1 + num2);
+    calculate(num1, num2, 'add');
 }
-    // SUBTRACT FUNCTION
+// SUBTRACT FUNCTION
 const performSubtraction = (num1, num2) => {
-    alert(num1 - num2);
+    calculate(num1, num2, 'subtract');
 }
-    // MULTIPLY FUNCTION
+// MULTIPLY FUNCTION
 const performMultiplication = (num1, num2) => {
-    alert(num1 * num2);
+    calculate(num1, num2, 'multiply');
 }
-    // DIVIDE FUNCTION
+// DIVIDE FUNCTION
 const performDivision = (num1, num2) => {
-    alert(num1 / num2);
+    calculate(num1, num2, 'divide');
 }
 
 // EXPORT THE FOUR PUBLIC FUNCTIONS
